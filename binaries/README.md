a6d65ff5308d614919ff5d8cf59d0729977fac69
root <root@alpinebuilder.home>
2025-05-23 11:32:36 +0200

    Added binary and manuel install

---
+++ binaries/README.md
@@ -0,0 +1,2 @@
+# Manual install of package.tar using IE3400 GUI
+
--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ docker image prune -af
 rm /opt/zabbixProxy/*.tar
 docker build -t zabbix-proxy-arm64:3.18-5.0.46 .
 docker save zabbix-proxy-arm64:3.18-5.0.46 -o zabbix-proxy-arm64.tar
-ioxclient package zabbix-proxy-arm64.tar ./
+ioxclient package zabbix-proxy-arm64.tar /opt/zabbixProxy/binaries
 rm -fr 2025*
